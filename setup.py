8b840a67cfafba1441b2b0daa8f1804df2c360d4
Nitrogen Dioxide <92470177+therealericsun@users.noreply.github.com>
2022-09-05 22:35:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
+from setuptools import setup
+
+setup(
+   name='deeparpes',
+   version='1.0',
+   description='A module to analyze ARPES imaging with deep autoencoders.',
+   author='Eric Sun',
+   author_email='therealericsun@gmail.com',
+   packages=['deeparpes'],  #same as name
+   install_requires=['scikit-learn', 'matplotlib', 'yellowbrick', 'numba', 'h5py', 'numpy', 'matplotlib', 'opencv-python', 'tensorflow', 'keras'], #external packages as dependencies
+   scripts=[
+            'scripts/cool',
+            'scripts/skype',
+           ]
+)
 
